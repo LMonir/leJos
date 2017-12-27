db3e3126d5dcfda1a6b4db2aff2f66a16b95f1d9
Lmonir <lmonir@htwsaar.de>
2017-12-27 11:48:59 +0100

    update

--- Grundfahrroboter/src/Main.java
+++ Grundfahrroboter/src/Main.java
@@ -13,19 +13,19 @@ public class Main {
 	public static void main(String[] args) {
 		LED led = LocalEV3.get().getLED();
 		//Roboter robo = new Roboter(49.4);
-		led.setPattern(7);
-
-		Audio audio = LocalEV3.get().getAudio();
-		audio.setVolume(20);
-		audio.playTone(1200, 100);
+		led.setPattern(6);
 		RegulatedMotor b;
 		RegulatedMotor c;	
 		b = new EV3LargeRegulatedMotor(MotorPort.B);
 		c = new EV3LargeRegulatedMotor(MotorPort.C);
 		Lightsensor licht = new Lightsensor(1);
-		PID pid = new PID(50, licht, 0.5, 0.2, 0.8, b, c);
-		pid.drivePID(150);
-		WaitFor.Degree(b, 1500, ">=");
+		PID pid = new PID(50, licht, 0.6, 1, 0.8, b, c);
+		Audio audio = LocalEV3.get().getAudio();
+		audio.setVolume(20);
+		audio.playTone(1200, 100);
+		led.setPattern(7);
+		pid.drivePID(200);
+		WaitFor.Degree(b, 2000, ">=");
 		pid.stopPID();
 		System.exit(0);
 	}
